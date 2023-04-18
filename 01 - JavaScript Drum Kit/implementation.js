d5af283b9b6d3db87df837c0f4022728e70dd999
rastko <vucen.rastko@gmail.com>
2023-04-18 19:59:40 +0200

    Implement drum kit

---
+++ 01 - JavaScript Drum Kit/implementation.js
@@ -0,0 +1,23 @@
+function playSound(e) {
+    const audio = this.document.querySelector(`audio[data-key="${e.key}"]`);
+    const key = this.document.querySelector(`.key[data-key="${e.key}"]`);
+
+    if (!audio || !key) return;
+
+    audio.currentTime = 0;
+    audio.play();
+
+    key.classList.add('playing');
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach((key) => {
+    key.addEventListener('transitionend', removeTransition);
+});
+
+window.addEventListener('keydown', playSound);
